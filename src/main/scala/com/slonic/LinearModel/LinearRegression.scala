ddad004cfd6e03f6b06e0436cdd845a02a1a3eb2
Artem Yankov <artem.yankov@gmail.com>
2015-02-03 21:20:14 -0800

    add non-vectorized implementation of gradient descent + add some benchmarks

--- src/main/scala/com/slonic/LinearModel/LinearRegression.scala
+++ src/main/scala/com/slonic/LinearModel/LinearRegression.scala
@@ -9,17 +9,41 @@ class LinearRegression extends BaseEstimator {
 
   var beta1: DenseVector[Double] = DenseVector[Double]()
 
+  def resetEstimator = beta1 = DenseVector[Double]()
+
   def fit(train: DenseMatrix[Double], y: DenseVector[Double],
-          gradDescent: Boolean = false, alpha: Double = 0.07): Unit = {
+          gradDescent: Boolean = false, alpha: Double = 0.07,
+          ep: Double = 0.000001): Unit = {
     if(gradDescent)
       fitGradientDescent(train, y)
     else
       fitMatrix(train, y)
   }
 
-  // Batch gradient descent solution
+  /**
+   * Below are 3 ways to fit the model for Linear regression.
+   * fitMatrix - is a closed form solution. It is simple and fast,
+   * but may perform badly if you have giant matrixes.
+   *
+   * fitGradientDescent is a way to solve it using gradient descent (duh)
+   *
+   * fitGradientDescentNonVec - same thing, but to calculate thetas insteas of
+   * matrix/vectors operations I used simple loops. That is just maybe slightly easy
+   * to understand if you don't remember how matrix multiplications works.
+   *
+   */
+
+  // Closed-form solution. The fastest and the simplest.
+  def fitMatrix(train: DenseMatrix[Double], y: DenseVector[Double]): Unit = {
+    val b1 = DenseMatrix.ones[Double](y.length, 1)
+    val trainI = DenseMatrix.horzcat(b1, train)
+    beta1 = inv(trainI.t * trainI) * (trainI.t * y)
+  }
+
+  // Batch gradient descent solution. Maybe used when training set is too big.
   def fitGradientDescent(train: DenseMatrix[Double], y: DenseVector[Double],
-                         alpha: Double = 0.007, ep: Double = 0.0001): Unit = {
+                         alpha: Double = 0.007, ep: Double = 0.000001,
+                         maxIter: Int = 1000000): Unit = {
 
     var thetha = DenseVector.zeros[Double](train.cols + 1)
     val t0 = DenseMatrix.ones[Double](y.length, 1)
@@ -29,25 +53,54 @@ class LinearRegression extends BaseEstimator {
     var err = (trainI * thetha) - y
     var e = 10.0
     var J = 100.0
-    var n_iter = 0
+    var nIter = 0
 
-    while(abs(J - e) > ep) {
-      n_iter += 1
+    while(abs(J - e) > ep && nIter <= maxIter) {
+      nIter += 1
       J = e
       thetha = thetha :- ((trainI.t * err) :* 1.0/m) * alpha
       err = (trainI * thetha) - y
       e = sum(pow(err, 2))
     }
 
-    println(s"Converged in $n_iter iterations")
+    println(s"Converged in $nIter iterations")
     beta1 = thetha
   }
 
-  // Closed-form solution
-  def fitMatrix(train: DenseMatrix[Double], y: DenseVector[Double]): Unit = {
-    val b1 = DenseMatrix.ones[Double](y.length, 1)
-    val trainI = DenseMatrix.horzcat(b1, train)
-    beta1 = inv(trainI.t * trainI) * (trainI.t * y)
+  // Non vectorized solution for gradient descent.
+  // Just in case you get lost with all matrix vectors multiplications
+  def fitGradientDescentNonVec(train: DenseMatrix[Double], y: DenseVector[Double],
+                         alpha: Double = 0.007, ep: Double = 0.000001,
+                         maxIter: Int = 1000000): Unit = {
+
+    var t0 = DenseMatrix.ones[Double](y.length, 1)
+    var t1 = DenseVector.zeros[Double](train.cols + 1)
+
+    // Add column for intercept
+    val trainI = DenseMatrix.horzcat(t0, train)
+
+    val m = train.rows
+
+    // Still multiplying matrix and vector here to calculate error
+    var err = (trainI * t1) - y
+    var e = 10.0
+    var J = 100.0
+    var nIter = 0
+
+    while(abs(J - e) > ep && nIter <= maxIter) {
+      nIter += 1
+      J = e
+      for (i <- 0 to m - 1) {
+        for (j <- 0 to trainI.cols - 1) {
+          t1(j) = t1(j) - alpha * 1.0 / m * (err(i) * trainI(i, j))
+        }
+      }
+
+      err = (trainI * t1) - y
+      e = sum(pow(err, 2))
+    }
+    println(s"Converged in $nIter iterations")
+    beta1 = t1
   }
 
   def predict(test: DenseMatrix[Double]): DenseVector[Double] = {
--- src/main/scala/com/slonic/examples/LinearRegression.scala
+++ src/main/scala/com/slonic/examples/LinearRegression.scala
@@ -8,16 +8,45 @@ import com.slonic.datasets.Iris
 object LinearRegression extends App {
   println("Linear regression example")
 
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) / 1000000 + "ms")
+    result
+  }
+
   val (labels, train) =  Iris.load()
   val clf = new LinearRegression()
   val X = train(::, 1 to 3)
 
-  // will be training to predict sepal length
+  // We will be learning to predict sepal length
   val y = train(::, 0)
-  clf.fit(X, y, gradDescent = true)
 
-  val yPred = clf.predict(X)
+  println("Fitting using closed form solution")
+  time { clf.fit(X, y) }
 
-  val error = sum(pow((yPred - y), 2)) / y.length
+  var yPred = clf.predict(X)
+
+  var error = sum(pow((yPred - y), 2)) / y.length
+  println(s"Standard Error: $error")
+
+  println("Fitting using Gradient descent")
+  clf.resetEstimator
+  time { clf.fit(X, y, gradDescent = true) }
+
+  yPred = clf.predict(X)
+
+  error = sum(pow((yPred - y), 2)) / y.length
+  println(s"Standard Error: $error")
+
+
+  println("Fitting using Gradient descent (non vectorized solution)")
+  clf.resetEstimator
+  time { clf.fitGradientDescentNonVec(X, y) }
+
+  yPred = clf.predict(X)
+
+  error = sum(pow((yPred - y), 2)) / y.length
   println(s"Standard Error: $error")
 }
