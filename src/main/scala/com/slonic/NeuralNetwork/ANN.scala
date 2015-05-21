fb6f9d90bc90512496a7b350ba52d684c0e28f93
Artem Yankov <artem.yankov@gmail.com>
2015-05-21 00:53:04 -0700

    fixes + saving and restoring models

--- src/main/scala/com/slonic/NeuralNetwork/ANN.scala
+++ src/main/scala/com/slonic/NeuralNetwork/ANN.scala
@@ -3,17 +3,19 @@ package com.slonic.NeuralNetwork
 import breeze.linalg._
 import breeze.numerics._
 import breeze.stats.distributions.Rand
-
 import scala.annotation.tailrec
+import java.io.File
 
-class ANN(inputSize: Int, nHidden: Int = 2, nOutput: Int = 10, alpha: Double = 0.8,
-          ep: Double = 0.000001, maxIter: Int = 10000, C: Double = 1) {
+class ANN(inputSize: Int, nHidden: Int = 2, nOutput: Int = 10,
+          ep: Double = 0.000001) {
 
   /* Constants for gradient checking */
   // Layer to check (running on all layers will take too long)
   val LAYER_CHK = 0
+
   // Number of neurons to check
   val NUM_NODES_CHK = 3
+
   // Number of weights to check (all of them by default)
   val NUM_WEIGHTS_CHK = 60
 
@@ -23,44 +25,26 @@ class ANN(inputSize: Int, nHidden: Int = 2, nOutput: Int = 10, alpha: Double = 0
   // Number of nodes in a hidden layer
   val LAYER_SIZE = 25
 
-  val lambda = alpha
-
-  def sigmoid(z: DenseVector[Double]): DenseVector[Double] = {
-    val ones = DenseVector.ones[Double](z.length)
-    ones / (exp(z) + 1.0)
-  }
+//  def sigmoid(z: DenseVector[Double]): DenseVector[Double] = {
+//    val ones = DenseVector.ones[Double](z.length)
+//    ones / (exp(-z) + 1.0)
+//  }
 
   def sigmoidGrad(a: DenseVector[Double]): DenseVector[Double] = a :* (-a + 1.0)
 
-  def inputLayer: DenseMatrix[Double] =
-    DenseMatrix.rand[Double](LAYER_SIZE, inputSize + 1, rand = Rand.gaussian)
-
-  def hiddenLayer: DenseMatrix[Double] =
-    DenseMatrix.rand[Double](LAYER_SIZE, LAYER_SIZE + 1, rand = Rand.gaussian)
-
-  def hiddenLayers: Array[DenseMatrix[Double]] =
-    (for(_ <- 0 to nHidden - 1) yield hiddenLayer).toArray
-
-  def outputLayer =
-    DenseMatrix.rand[Double](nOutput, LAYER_SIZE + 1, rand = Rand.gaussian)
+  def createLayer(n: Int): DenseMatrix[Double] = n match {
+    // first hidden layer
+    case 0 => DenseMatrix.rand[Double](LAYER_SIZE, inputSize + 1, rand = Rand.gaussian)
+    // output layer
+    case `nHidden` => DenseMatrix.rand[Double](nOutput, LAYER_SIZE + 1, rand = Rand.gaussian)
+    // other hidden layers
+    case _ => DenseMatrix.rand[Double](LAYER_SIZE, LAYER_SIZE + 1, rand = Rand.gaussian)
+  }
 
   def zeroCopy(mx: Array[DenseMatrix[Double]]) =
     mx.map(m => DenseMatrix.zeros[Double](m.rows, m.cols))
 
-  val weights: Array[DenseMatrix[Double]] =
-    (inputLayer +: hiddenLayers) ++ (outputLayer +: Array())
-
-  // Forward propagation
-  @tailrec
-  final def h(x: DenseVector[Double], thetas: Array[DenseMatrix[Double]],
-              aVec: List[DenseVector[Double]] = List()): (DenseVector[Double], List[DenseVector[Double]])= {
-    if(thetas.isEmpty)
-      (x(1 to -1), aVec)
-    else {
-      val a = sigmoid(thetas.head * x).toArray
-      h(DenseVector(1.0 +: a), thetas.tail, aVec :+ x)
-    }
-  }
+  var weights: Array[DenseMatrix[Double]] = (0 to nHidden).map(createLayer).toArray
 
   def binarize(ys: DenseVector[Int]): DenseMatrix[Double] = {
     val kl = ys.toArray.toSet
@@ -70,10 +54,11 @@ class ANN(inputSize: Int, nHidden: Int = 2, nOutput: Int = 10, alpha: Double = 0
   }
 
   def loss(y: DenseVector[Double], yPred: DenseVector[Double]): DenseVector[Double] =
-    (y :* log(yPred)) :+ ((-y + 1.0) :* log(-yPred + 1.0))
+    (-y :* log(yPred)) :- ((-y + 1.0) :* log(-yPred + 1.0))
 
-  def J(x: DenseMatrix[Double], thetas: Array[DenseMatrix[Double]], y: DenseMatrix[Double], regularize: Boolean = true): Double = {
-    val j = (-1.0/x.rows) * sum(for {
+  def J(x: DenseMatrix[Double], thetas: Array[DenseMatrix[Double]], y: DenseMatrix[Double], regularize: Boolean = true,
+        lambda: Double = 0.8): Double = {
+    val j = (1.0/x.rows) * sum(for {
       i <- 0 to x.rows - 1
       yPred = h(x(i, ::).inner, thetas)._1
       yi = y(i, ::).inner
@@ -85,21 +70,44 @@ class ANN(inputSize: Int, nHidden: Int = 2, nOutput: Int = 10, alpha: Double = 0
       j
   }
 
+ // Forward propagation
+  @tailrec
+  final def h(x: DenseVector[Double], thetas: Array[DenseMatrix[Double]],
+              aVec: List[DenseVector[Double]] = List()): (DenseVector[Double], List[DenseVector[Double]])= {
+    if(thetas.isEmpty)
+      (x(1 to -1), aVec)
+    else {
+      val a = sigmoid(thetas.head * x).toArray
+      h(DenseVector(1.0 +: a), thetas.tail, aVec :+ x(1 to -1))
+    }
+  }
+
+//  @tailrec
+//  final def h(x: DenseMatrix[Double], thetas: Array[DenseMatrix[Double]]): DenseMatrix[Double]= {
+//    if(thetas.isEmpty)
+//      x(::, 1 to -1)
+//    else {
+//      var a: DenseMatrix[Double] = sigmoid(thetas.head * x.t)
+//      a = DenseMatrix.horzcat(DenseMatrix.ones[Double](a.rows, 1), a)
+//      h(a, thetas.tail)
+//    }
+//  }
+
   def backpropagate(train: DenseMatrix[Double], ys: DenseMatrix[Double],
                     weights: Array[DenseMatrix[Double]]): Array[DenseMatrix[Double]] = {
+
     val D = zeroCopy(weights)
+    D.foreach(d => d(::, 0) := 1.0)
 
     for (i <- 0 to train.rows - 1) {
-      var (a, aVec) = h(train(i, ::).inner, weights) // a4
-      var delta = a - ys(i, ::).inner // delta4
+      var (a, aVec) = h(train(i, ::).inner, weights) // get predictions and activation for every layer
+      var delta = a - ys(i, ::).inner // delta(L)
 
       for (l <- weights.length - 1 to 0 by - 1) {
         a = aVec(l)
-        val t0 = D(l)(::, 0)
-        D(l) = D(l) + (delta * a.t) // delta(l+1) * a.t(l)
-        D(l)(::, 0) := t0 :* delta  // bias deltas
+        D(l)(::, 0) := D(l)(::, 0) :* delta  // bias deltas
+        D(l)(::, 1 to -1) := D(l)(::, 1 to -1) + (delta * a.t) // delta(l+1) * a.t(l)
 
-        a = a.slice(1, a.length)
         delta = (weights(l).t(1 to -1, ::) * delta) :* sigmoidGrad(a) // delta(l) = theta(l) * delta(l+1) * a'
       }
     }
@@ -107,16 +115,19 @@ class ANN(inputSize: Int, nHidden: Int = 2, nOutput: Int = 10, alpha: Double = 0
     D.map(_ :/ train.rows.toDouble)
   }
 
-  def fit(train: DenseMatrix[Double], y: DenseVector[Int], maxIter: Int = 100000,
-          checkGradients: Boolean = false): Unit = {
+  def train(train: DenseMatrix[Double], y: DenseVector[Int], maxIter: Int = 10000,
+            checkGradients: Boolean = false, restoreModel: Boolean = false,
+            lambda: Double = 0.8): Unit = {
 
     val ys = binarize(y)
     val bias = DenseMatrix.ones[Double](train.rows, 1)
     val trainI = DenseMatrix.horzcat(bias, train)
 
-    var cost = J(trainI, weights, ys)
-    val m = trainI.rows
+    if(restoreModel)
+      weights = readWeights
+    var cost = J(trainI, weights, ys, lambda = 0.8)
     var nIter = 0
+
 
     // Backpropagation
     println(s"Initial cost $cost")
@@ -138,16 +149,21 @@ class ANN(inputSize: Int, nHidden: Int = 2, nOutput: Int = 10, alpha: Double = 0
       // Update weights
       for (l <- 0 to weights.length - 1) {
         weights(l)(::, 0) := D(l)(::, 0)
-        weights(l)(::, 1 to -1) := weights(l)(::, 1 to -1) + D(l)(::, 1 to -1) * alpha
+        weights(l)(::, 1 to -1) := weights(l)(::, 1 to -1) - D(l)(::, 1 to -1) * lambda
       }
 
-      cost = J(trainI, weights, ys)
+      cost = J(trainI, weights, ys, lambda = 0.8)
       println(s"Cost $cost at epoch $nIter")
+      if(nIter % 100 == 0) {
+        println(s"Saving the weights at epoch $nIter")
+        saveWeights(weights)
+      }
+
       nIter += 1
     }
 
+    saveWeights(weights)
   }
-
 
   // Check gradients for a given layer, nodes and weights
   def areGradsOk(trainI: DenseMatrix[Double], ys: DenseMatrix[Double], D: Array[DenseMatrix[Double]],
@@ -175,6 +191,24 @@ class ANN(inputSize: Int, nHidden: Int = 2, nOutput: Int = 10, alpha: Double = 0
 
   }
 
-  //def predict(test: DenseMatrix[Double]): DenseMatrix[Double] = h(test, weights)
+  def saveWeights(weights: Array[DenseMatrix[Double]]) =
+    weights.zipWithIndex.foreach{case (ws, i) =>  csvwrite(new File(s"models/theta${i+1}.csv"), ws)}
+
+  def readWeights: Array[DenseMatrix[Double]] =
+    (0 to nHidden).map(i => csvread(new File(s"models/theta${i+1}.csv"))).toArray
+
+
+  def predictProba(test: DenseMatrix[Double]): DenseMatrix[Double] =
+    h(test, weights)
+
+//  def predict(test: DenseMatrix[Double]): List[Int] = {
+//    val yPred = h(test, weights)
+//    for {
+//      i <- 0 to yPred.rows - 1
+//      y = yPred(i,::).inner
+//    } yield y.findAll(_ == max(y))
+//  }
+
+
 
 }
--- src/main/scala/com/slonic/examples/NeuralNetworks.scala
+++ src/main/scala/com/slonic/examples/NeuralNetworks.scala
@@ -1,27 +1,21 @@
 package com.slonic.examples
 
-import breeze.linalg._
 import com.slonic.NeuralNetwork.{ANN, Perceptron}
 import com.slonic.datasets.{Digits5k, Digits}
 
 object NeuralNetworks extends App {
-  println("Perceptron example")
-  val predLabel = 9
   val (labels, train) = Digits5k.load()
-  //val y = labels.map(l => if(l == predLabel) 1 else 0)
 
+//  println("Perceptron example")
+//  val predLabel = 9
+  //val y = labels.map(l => if(l == predLabel) 1 else 0)
 //  val perceptron = new Perceptron()
 //  perceptron.fit(train, y)
 //  val yPred = perceptron.predict(train)
 //  println(s"Weights: ${perceptron.weights}")
 
-  // 1 layer 5k = 3.2 lost
-  // 2 layer 5k = 2.8 lost (no reg)/ 2.32 at epoch 1000
+  println("Multi-layer neural net example")
   val nn = new ANN(inputSize = train.cols, nHidden = 1)
-
- // val ones = DenseVector.ones[Double](train.rows)
- // val H = DenseMatrix.eye[Double](train.rows) - (ones * ones.t)
- // val trainC = H * train
 
  // Substract mean
  // for(i <- 0 to train.rows - 1) {
@@ -29,6 +23,7 @@ object NeuralNetworks extends App {
  //   train(i, ::).inner := v - (sum(v) / v.length)
  // }
 
-  nn.fit(train, labels, checkGradients = true)
+  nn.train(train, labels, checkGradients = false, maxIter = 5)
+  //val yPred = nn.predictProba(train)
   //val yPred = nn.predict(train)
 }
